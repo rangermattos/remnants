9ff9ae62d005ba9da8b3b4c700d999bdd54b53e7
Brandon Guyton <bpgtechn@gmail.com>
2016-11-30 23:53:12 -0600

    fixed units causing lag by not setting tiles canwalk bool to false when buildings are built

--- Remnants/Models/Items/Entity.cs
+++ Remnants/Models/Items/Entity.cs
@@ -1,6 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Content;
 
 namespace Remnants.Models
